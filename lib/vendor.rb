0dcc8094dec93b526fad966218738eeaa3dfb300
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-26 10:37:22 -0700

    Iteration 1 passes rake

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,10 +1,17 @@
 class Vendor
-    attr_reader :name,
-                :inventory
+  attr_reader :name,
+              :inventory
 
   def initialize(name)
-      @name = name
-      @inventory = Hash.new(0)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+  
+  def check_stock(item)
+    @inventory[item]
   end
 
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -8,7 +8,6 @@ class VendorTest < Minitest::Test
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @vendor = Vendor.new("Rocky Mountain Fresh")
-
   end
 
   def test_it_exists
@@ -19,38 +18,20 @@ class VendorTest < Minitest::Test
     assert_equal "Rocky Mountain Fresh", @vendor.name
     assert_equal ({}), @vendor.inventory
   end
+
+  def test_can_items_can_be_stock_and_returned_in_inventory_hash
+    assert_equal 0, @vendor.check_stock(@item1)
+    assert_equal 0, @vendor.check_stock(@item_2)
+
+    @vendor.stock(@item1, 30)
+    assert_equal ({@item1 => 30}), @vendor.inventory
+
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+    assert_equal ({@item1 => 55, @item2 => 12}), @vendor.inventory
+  end
 end
-
-
-# pry(main)> item2.name
-# #=> "Tomato"
-#
-# pry(main)> item2.price
-# #=> 0.50
-#
-# pry(main)> vendor.name
-# #=> "Rocky Mountain Fresh"
-#
-# pry(main)> vendor.inventory
-# #=> {}
-#
-# pry(main)> vendor.check_stock(item1)
-# #=> 0
-#
-# pry(main)> vendor.stock(item1, 30)
-#
-# pry(main)> vendor.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-#
-# pry(main)> vendor.check_stock(item1)
-# #=> 30
-#
-# pry(main)> vendor.stock(item1, 25)
-#
-# pry(main)> vendor.check_stock(item1)
-# #=> 55
-#
-# pry(main)> vendor.stock(item2, 12)
-#
-# pry(main)> vendor.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
